5dea6b25ff59d7ed947d921cb826d520433dcea1
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-09 03:17:22 -0500

    converting to one file mode.

--- index.php
+++ index.php
@@ -5,7 +5,38 @@ define('TEMPLATE_DIR', ROOT_DIR.'templates/');
 define('PLUGIN_DIR', ROOT_DIR.'plugins/');
 define('INCLUDE_DIR', ROOT_DIR.'includes/');
 
-include(INCLUDE_DIR.'leafcms.php');
+class Leaf {
+  
+  public function returnError($code) {
+    switch($code) {
+      case 404:
+        $message = "404 Error. The page could not be found!"; // @todo: set to config value later
+        $template = $this->getTemplate("error")->setBinding("message", $message);
+        return $template;
+    }
+  }
+  
+  public function runPage($page) {
+    $config = include(CONFIG_DIR.'pages.php');
+    $output = '';
+    if(isset($config['page_templates'][$page])) {
+      $output = $this->getTemplate($config['page_templates'][$page];
+    }
+    else {
+      // return 404.
+      $html = $this->returnError('404');
+      return false; // error.
+    }
+    $this->output = $output;
+    return true;
+  }
+  
+  public function runExtensions($extension='all') {
+  
+  }
+  
+}
+
 
 $leaf = new LeafCMS;
 
